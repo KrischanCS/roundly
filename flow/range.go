7e8af2fbf4d7d5bb1d2172608e167bb37602e022
Christian Schulz <mail@chris-s.eu>
2024-12-23 09:54:31 +0100

    Pre-renders ranges for performance reasons

--- flow/range.go
+++ flow/range.go
@@ -3,34 +3,41 @@ package flow
 import (
 	"iter"
 
+	"github.com/valyala/bytebufferpool"
+
 	"github.com/ch-schulz/htmfunc"
 )
 
 func Range[T any](items []T, component func(int, T) htmfunc.Element) htmfunc.Element {
-	return func(w htmfunc.Writer) error {
-
-		for i, e := range items {
-			err := component(i, e)(w)
-			if err != nil {
+	buf := bytebufferpool.Get()
+	for i, e := range items {
+		err := component(i, e)(buf)
+		if err != nil {
+			return func(_ htmfunc.Writer) error {
 				return err
 			}
 		}
+	}
 
-		return nil
+	return func(w htmfunc.Writer) error {
+		_, err := buf.WriteTo(w)
+		return err
 	}
 }
 
 func RangeInt(limit int, component func(int) htmfunc.Element) htmfunc.Element {
-	return func(w htmfunc.Writer) error {
-
-		for i := range limit {
-			err := component(i)(w)
-			if err != nil {
+	buf := bytebufferpool.Get()
+	for i := range limit {
+		err := component(i)(buf)
+		if err != nil {
+			return func(_ htmfunc.Writer) error {
 				return err
 			}
 		}
-
-		return nil
+	}
+	return func(w htmfunc.Writer) error {
+		_, err := buf.WriteTo(w)
+		return err
 	}
 }
 
@@ -41,15 +48,18 @@ func RangeIter(seq iter.Seq2[int, int], component func(int, int) htmfunc.Element
 		}
 	}
 
-	return func(w htmfunc.Writer) error {
-
-		for t1, t2 := range seq {
-			err := component(t1, t2)(w)
-			if err != nil {
+	buf := bytebufferpool.Get()
+	for t1, t2 := range seq {
+		err := component(t1, t2)(buf)
+		if err != nil {
+			return func(_ htmfunc.Writer) error {
 				return err
 			}
 		}
+	}
 
-		return nil
+	return func(w htmfunc.Writer) error {
+		_, err := buf.WriteTo(w)
+		return err
 	}
 }
