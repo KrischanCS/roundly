0be086646eb1d3257f1762c1ac013e7a21aa9a7e
Christian Schulz <mail@chris-s.eu>
2025-09-13 12:00:23 +0200

    range: combine ranger and rangerOpts

--- logic/range.go
+++ logic/range.go
@@ -8,99 +8,63 @@ import (
 )
 
 type Iterable[E any] interface {
-	~[]E |
-		iter.Seq[E] |
-		iter.Seq2[int, E]
+	~[]E | iter.Seq[E] | iter.Seq2[int, E]
 }
 
 // Range creates a roundly.Element for each item if items.
 func Range[E any, I Iterable[E]](items I, component func(int, E) roundly.Element) roundly.Element {
-	return func(w roundly.Writer, opts *roundly.RenderOptions) error {
-		if items == nil {
+	if items == nil {
+		return func(_ roundly.Writer, _ *roundly.RenderOptions) error {
 			return nil
 		}
-
-		if opts == nil {
-			return ranger[E, I](w, items, component)
-		}
-
-		return rangeOpts[E, I](w, items, component, opts)
 	}
-}
-
-func ranger[E any, I Iterable[E]](
-	w roundly.Writer,
-	items I,
-	component func(int, E) roundly.Element,
-) error {
 
 	switch items := any(items).(type) {
 	case []E:
-		for i, e := range items {
-			err := component(i, e).RenderElement(w)
-			if err != nil {
-				return err
-			}
-		}
+		return rangeSlice(items, component)
 	case iter.Seq2[int, E]:
-		for i, e := range items {
-			err := component(i, e).RenderElement(w)
-			if err != nil {
-				return err
-			}
-		}
+		return rangeSeq2(items, component)
 	case iter.Seq[E]:
-		i := 0
-		for e := range items {
-			err := component(i, e).RenderElement(w)
-			if err != nil {
-				return err
-			}
-			i++
-		}
+		return rangeSeq(items, component)
 	default:
 		panic(fmt.Sprintf("ranger called with unsupported type: %T", items))
 	}
-
-	return nil
 }
 
-func rangeOpts[E any, I Iterable[E]](
-	w roundly.Writer,
-	items I,
-	component func(int, E) roundly.Element,
-	opt *roundly.RenderOptions,
-) error {
-
-	switch items := any(items).(type) {
-	case []E:
-		for i, e := range items {
-			err := component(i, e).RenderElementWithOptions(w, opt)
-			if err != nil {
-				return err
-			}
-		}
-	case iter.Seq2[int, E]:
-		for i, e := range items {
-			err := component(i, e).RenderElementWithOptions(w, opt)
-			if err != nil {
-				return err
-			}
-		}
-	case iter.Seq[E]:
-		i := 0
-		for e := range items {
-			err := component(i, e).RenderElementWithOptions(w, opt)
-			if err != nil {
-				return err
-			}
-			i++
-		}
-	default:
-		panic(fmt.Sprintf("ranger called with unsupported type: %T", items))
+func rangeSlice[E any](items []E, component func(int, E) roundly.Element) roundly.Element {
+	elements := make([]roundly.Element, len(items))
+	for i, item := range items {
+		elements[i] = component(i, item)
 	}
 
-	return nil
+	return func(w roundly.Writer, opts *roundly.RenderOptions) error {
+		return Group(elements...)(w, opts)
+	}
+}
+
+func rangeSeq2[E any](items iter.Seq2[int, E], component func(int, E) roundly.Element) roundly.Element {
+	elements := make([]roundly.Element, 0, 16)
+	for i, item := range items {
+		elements = append(elements, component(i, item))
+	}
+
+	return func(w roundly.Writer, opts *roundly.RenderOptions) error {
+		return Group(elements...)(w, opts)
+	}
+}
+
+func rangeSeq[E any](items iter.Seq[E], component func(int, E) roundly.Element) roundly.Element {
+	elements := make([]roundly.Element, 0, 16)
+	i := 0
+
+	for item := range items {
+		elements = append(elements, component(i, item))
+		i++
+	}
+
+	return func(w roundly.Writer, opts *roundly.RenderOptions) error {
+		return Group(elements...)(w, opts)
+	}
 }
 
 // RangeInt creates a roundly.Element for each int in [0, limit).
